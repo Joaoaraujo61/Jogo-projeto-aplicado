6b208c315816ed3cede012db7ddea9bb78eefbc6
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2026-05-24 17:21:03 -0300

    classe botao criada e refatoracao POO

---
+++ src/Botao.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class Botao {
+    private ImageIcon icone;
+    private JButton botaoClicavel;
+    private int largura;
+    private int altura;
+    private int posicaoX;
+    private int posicaoY;
+
+    public Botao(ImageIcon icone,int posicaoX,int posicaoY){
+        this.icone = icone;
+        this.largura = icone.getIconWidth();
+        this.altura = icone.getIconHeight();
+        this.posicaoY = posicaoY;
+        this.posicaoX = (1280 - largura) / 2;//
+        //int y = (720 - alturaBotao) / 2;
+
+        botaoClicavel = new JButton(icone);
+        botaoClicavel.setBounds(posicaoX, posicaoY, largura, altura);
+
+        botaoClicavel.setBorderPainted(false);
+        botaoClicavel.setContentAreaFilled(false);
+        botaoClicavel.setFocusPainted(false);
+        botaoClicavel.setOpaque(false);
+
+    }
+
+    public JButton getBotaoClicavel() {
+        return botaoClicavel;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,40 @@
 import java.awt.Image;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ImageIcon fundo = new ImageIcon(
-                Main.class.getResource("/assets/fundoMenu.png"));
-        ImageIcon comecarIcon = new ImageIcon(
-                Main.class.getResource("/assets/botaoComecar.png"));
-        ImageIcon continuarIcon = new ImageIcon(
-                Main.class.getResource("/assets/botaoContinuar.png"));
+        ImageIcon iconeComecar = new ImageIcon(Main.class.getResource("/assets/botaoComecar.png"));
+        ImageIcon iconeContinuar = new ImageIcon(Main.class.getResource("/assets/botaoContinuar.png"));
+        ImageIcon fundo = new ImageIcon(Main.class.getResource("/assets/fundoMenu.png"));
+        ImageIcon nomeJogo = new ImageIcon(Main.class.getResource("/assets/RotaDeFuga.png"));
 
         JFrame frame = new JFrame("Menu");
+
+        //fundo
+        JLabel labelFundo = new JLabel(fundo);
+        labelFundo.setLayout(null);
+
+        //nome Jogo
+        JLabel labelNome = new JLabel(nomeJogo);
+        labelNome.setBounds(410, 40, 459, 51);
+
+        //botoes
+        Botao botaoComecar = new Botao(iconeComecar,490,380);
+        Botao botaoContinuar = new Botao(iconeContinuar,472, 504);
+
+        //adiciona componentes
+        labelFundo.add(labelNome);
+        labelFundo.add(botaoContinuar.getBotaoClicavel());
+        labelFundo.add(botaoComecar.getBotaoClicavel());
+
+        frame.setContentPane(labelFundo);
 
         frame.setSize(1280, 720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-
-        JLabel labelFundo = new JLabel(fundo);
-        labelFundo.setLayout(null);
-
-        JButton botaoComecar = new JButton(comecarIcon);
-        JButton botaoContinuar = new JButton(continuarIcon);
-        
-        int larguraBotao = continuarIcon.getIconWidth();
-        int alturaBotao = continuarIcon.getIconHeight();
-
-        int x = (1280 - larguraBotao) / 2;
-        int y = (720 - alturaBotao) / 2;
-
-
-        botaoContinuar.setBounds(x, y, larguraBotao, alturaBotao);
-        botaoComecar.setBounds(x, 130, larguraBotao, alturaBotao);
-
-        botaoContinuar.setBorderPainted(false);
-        botaoContinuar.setContentAreaFilled(false);
-        botaoContinuar.setFocusPainted(false);
-        botaoContinuar.setOpaque(false);
-
-        botaoComecar.setBorderPainted(false);
-        botaoComecar.setContentAreaFilled(false);
-        botaoComecar.setFocusPainted(false);
-        botaoComecar.setOpaque(false);
-
-        labelFundo.add(botaoContinuar);
-        labelFundo.add(botaoComecar);
-
-        frame.setContentPane(labelFundo);
-
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 }
