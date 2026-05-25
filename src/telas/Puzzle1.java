93ec1f86c3d79e1dcdc98bae9b48729167f052c8
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2026-05-24 23:30:32 -0300

    Puzzle 1iniciado, animacao telefone

--- src/telas/Puzzle1.java
+++ src/telas/Puzzle1.java
@@ -1,15 +1,44 @@
 package telas;
 
+import componentes.Botao;
+
 import javax.swing.*;
 
 public class Puzzle1 extends JPanel {
+    private ImageIcon fundo;
+    private JLabel labelFundo;
+    private ImageIcon imgTelefone;
+    private ImageIcon imgTelefoneTocando;
+    private Botao telefoneBtn;
+    private Botao telefoneTocandoBtn;
+
     public Puzzle1(JFrame frame) {
-        setLayout(null);
+        //Fundo
+        this.fundo = new ImageIcon(MenuInicial.class.getResource("/assets/salaDeEstar-pixilart.png"));
+        this.labelFundo = new JLabel(fundo);
+        labelFundo.setBounds(0, 0, 1280, 720);
+        labelFundo.setLayout(null);
 
-        JLabel texto = new JLabel("FASE 1");
+        this.imgTelefone = new ImageIcon(MenuInicial.class.getResource("/assets/telefone.png"));
+        this.telefoneBtn = new Botao(imgTelefone,970,200);
 
-        texto.setBounds(500, 300, 200, 50);
+        this.imgTelefoneTocando = new ImageIcon(MenuInicial.class.getResource("/assets/telefoneTocando.png"));
+        this.telefoneTocandoBtn = new Botao(imgTelefone,970,200);
 
-        add(texto);
+        labelFundo.add(telefoneBtn.getBotaoClicavel());
+        add(labelFundo);
+    }
+
+    public void tocarTelefone(){
+        Timer timer = new Timer(500, e ->{
+            JButton botao = telefoneBtn.getBotaoClicavel();
+            Icon iconeAtual = botao.getIcon();
+            if (iconeAtual == imgTelefone) {
+                botao.setIcon(imgTelefoneTocando);
+            } else {
+                botao.setIcon(imgTelefone);
+            }
+        });
+        timer.start();
     }
 }
