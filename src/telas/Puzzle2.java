376a42d00e7237b03f856067c34f7422a10cd094
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2026-06-12 21:20:59 -0300

    novos assets adicionados

--- src/telas/Puzzle2.java
+++ src/telas/Puzzle2.java
@@ -1,5 +1,6 @@
 package telas;
 
+import componentes.Botao;
 import componentes.CaixaDialogo;
 import componentes.Personagem;
 import componentes.Telefone;
@@ -15,8 +16,15 @@ public class Puzzle2 extends JPanel {
     public Puzzle2(FrameJanela frame) {
         this.frame = frame;
         //Fundo
-        ImageIcon fundo = new ImageIcon(Puzzle1.class.getResource("/assets/QuartRosalngela.png"));
+        ImageIcon fundo = new ImageIcon(Puzzle2.class.getResource("/assets/QuartRosalngelaSemVestido.png"));
+        ImageIcon vestidoRasgado = new ImageIcon(Puzzle2.class.getResource("/assets/VestidoRasgadoNoCabide.png"));
+        Botao botaoVestidoRasgado = new Botao(vestidoRasgado,1070, 300);
+        botaoVestidoRasgado.getBotaoClicavel().setBorderPainted(false);
+        botaoVestidoRasgado.getBotaoClicavel().setContentAreaFilled(false);
+        botaoVestidoRasgado.getBotaoClicavel().setFocusPainted(false);
+
         this.labelFundo = new JLabel(fundo);
+        labelFundo.add(botaoVestidoRasgado.getBotaoClicavel());
         labelFundo.setBounds(0, 0, 1280, 720);
         labelFundo.setLayout(null);
 
