166350ac2395d065cace8e252b4ad197c200c792
Biblioteca <Biblioteca@UNDF0621BCE7>
2026-06-26 15:30:20 -0300

    fios da esquerda add

--- src/telas/Puzzle3Jogavel.java
+++ src/telas/Puzzle3Jogavel.java
@@ -22,6 +22,9 @@ public class Puzzle3Jogavel extends JPanel {
     private JLabel labelFundo;
     private Botao telefoneBtn;
     private JLabel labelFiosDireita;
+    private JLabel labelFioVerde;
+    private JLabel labelFioVermelho;
+    private JLabel labelFioAzul;
    
 
     public Puzzle3Jogavel(FrameJanela frame) {
@@ -29,22 +32,44 @@ public class Puzzle3Jogavel extends JPanel {
         
         setLayout(null);
         setPreferredSize(new Dimension(1280, 720));
+        
+     
+        ImageIcon imgFiosDireita = new ImageIcon(getClass().getResource("/assets/fiosDireita.png"));
+        this.labelFiosDireita = new JLabel(imgFiosDireita);
+        labelFiosDireita.setBounds(358, 158, imgFiosDireita.getIconWidth(), imgFiosDireita.getIconHeight());
+        this.add(labelFiosDireita); 
+        
+        ImageIcon imgfioAzul= new ImageIcon(getClass().getResource("/assets/fioAzul.png"));
+        this.labelFioAzul = new JLabel(imgfioAzul);
+        labelFioAzul.setBounds(358, 260, imgfioAzul.getIconWidth(), imgfioAzul.getIconHeight());
+        this.add(labelFioAzul); 
+        
+        ImageIcon imgFioVerde = new ImageIcon(getClass().getResource("/assets/fioVerde.png"));
+        this.labelFioVerde = new JLabel(imgFioVerde);
+        labelFioVerde.setBounds(358, 350, imgFioVerde.getIconWidth(), imgFioVerde.getIconHeight());
+        this.add(labelFioVerde); 
+        
       
-        // Imagem de Fundo 
+        ImageIcon imgfioVermelho= new ImageIcon(getClass().getResource("/assets/fioVermelho.png"));
+        this.labelFioVermelho = new JLabel(imgfioVermelho);
+        labelFioVermelho.setBounds(358, 420, imgfioVermelho.getIconWidth(), imgfioVermelho.getIconHeight());
+        this.add(labelFioVermelho); 
+      
+        // 3. IMAGEM DE FUNDO (Adicionada por último para ir para trás de tudo)
         ImageIcon fundo = new ImageIcon(MenuInicial.class.getResource("/assets/FundoCaixadeFios.png"));
         this.labelFundo = new JLabel(fundo);
         labelFundo.setBounds(0, 0, 1280, 720);
         this.add(labelFundo); 
   
+        // Garantia absoluta usando ZOrder: 
+        // Empurra o fundo para o último lugar da fila (atrás)
         this.setComponentZOrder(labelFundo, this.getComponentCount() - 1);
         
-        ImageIcon imgFiosDireita = new ImageIcon(getClass().getResource("/assets/fiosDireita.png"));
-        this.labelFiosDireita = new JLabel(imgFiosDireita);
-        labelFiosDireita.setBounds(358, 158, imgFiosDireita.getIconWidth(), imgFiosDireita.getIconHeight());
-        labelFiosDireita.setLayout(null);
-        labelFundo.add(labelFiosDireita);
-        
-      
+        // Coloca os fios nas posições da frente (índice 0 e 1)
+        this.setComponentZOrder(labelFioAzul, 0);
+        this.setComponentZOrder(labelFioVerde, 1);
+        this.setComponentZOrder(labelFioVermelho, 2);
+        this.setComponentZOrder(labelFiosDireita, 3);
     } 
      
     void mudarTela() {
