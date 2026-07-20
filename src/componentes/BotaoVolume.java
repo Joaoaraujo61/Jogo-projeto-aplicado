dd7d7a26a978627c6e948ddca865171e727414a5
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2026-07-20 02:16:22 -0300

    sound on/off asset

--- src/componentes/BotaoVolume.java
+++ src/componentes/BotaoVolume.java
@@ -4,22 +4,33 @@ import javax.swing.*;
 
 public class BotaoVolume {
     private Botao botaoSom;
+    private ImageIcon somIcone;
+    private ImageIcon iconeSomOn;
+    private ImageIcon iconeSomOff;
 
     public BotaoVolume(){
-        ImageIcon somIcone = new ImageIcon(getClass().getResource("/assets/mute.png"));
+        this.iconeSomOn = new ImageIcon(getClass().getResource("/assets/sound-on.png"));
+        this.iconeSomOff = new ImageIcon(getClass().getResource("/assets/sound-off.png"));
+
+        this.somIcone = iconeSomOn;
         this.botaoSom = new Botao(somIcone, 30, 40);
 
-        botaoSom.getBotaoClicavel().addActionListener(e->{
+        botaoSom.getBotaoClicavel().addActionListener(e -> {
+            JButton botao = botaoSom.getBotaoClicavel();
+
             if (GerenciadorSom.getVolumeAtual() > 0){
                 GerenciadorSom.ajustarVolumeTrilhaAtual(0);
-            }else{
+                somIcone = iconeSomOff;
+            } else {
                 GerenciadorSom.ajustarVolumeTrilhaAtual(0.35f);
+                somIcone = iconeSomOn;
             }
 
+            botao.setIcon(somIcone); // <-- isso é o que faltava
         });
     }
+
     public JButton getBotaoSom() {
         return botaoSom.getBotaoClicavel();
     }
-
 }
